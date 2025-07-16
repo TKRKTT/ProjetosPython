4b18c60ce6907a90170df457511be0788b0fb179
Richard Gabryell Caetano Barbosa <richardgabryellcaetano@gmail.com>
2025-07-16 02:25:11 -0300

    add exercicio02.py for basic age verification functionality

---
+++ exercício02.py
@@ -0,0 +1,14 @@
+# exercicio02.py
+
+nome = input("Digite seu nome: ")
+
+idade = float(input("Digite sua idade: "))
+
+if idade >= 18:
+    print(f" --- Olá {nome}, você tem {idade} anos e é maior de idade ---")
+else:
+    print(f" --- Olá {nome}, vocẽ tem {idade} anos e é menor de idade ---")
+
+# fim do código
+
+# Exercício 02 - Python Básico com Condicional
