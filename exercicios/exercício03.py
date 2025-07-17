f4869d180c39def18464ce26ef8bb7cd60d61115
Richard Gabryell Caetano Barbosa <richardgabryellcaetano@gmail.com>
2025-07-17 04:55:53 -0300

    refactor output formatting in exercicio03.py for improved readability

--- exercicios/exercício03.py
+++ exercicios/exercício03.py
@@ -2,7 +2,7 @@
 
 frutas = ["maçã", "banana", "laranja", "melão", "abacaxi"]
 
-print("Lista de frutas favoritas são:", frutas)
+print("\n Lista de frutas favoritas são:", frutas)
 
 primeira_fruta = frutas[0]
 segunda_fruta = frutas[1]
@@ -10,18 +10,28 @@ segunda_fruta = frutas[1]
 print(f"A primeira fruta é: {primeira_fruta}, e a segunda fruta é: {segunda_fruta}")
 
 frutas.append("uva")
-print("Lista de frutas atualizada:", frutas)
+print("\nLista de frutas atualizada:", frutas)
 
 frutas.remove("banana")
-print("Lista de frutas após remoção da banana:", frutas)
+print("\nLista de frutas após remoção da banana:", frutas)
 
 quantidade_frutas = len(frutas)
-print(f"Quantidade de frutas na lista: {quantidade_frutas}")
+print(f"\n Quantidade de frutas na lista: {quantidade_frutas}")
+
+frutas.insert(2, "Kiwi")
+print("\nLista de frutas após inserção do Kiwi:", frutas)
+
+del frutas[3]
+print("Lista de frutas após remoção do elemnto do indice 3:", frutas)
+
+if "morango" in frutas:
+    print("\n --- Morango está na lista de frutas ---")
+else:
+    print("\n --- Morango não está na lista de frutas ---")
 
 print("\n --- Percorendo as frutas na lista ---")
 for fruta in frutas:
-    print(f"Frutas: {frutas}")
-    print("fim exercício03.py")
+    print(f"- {frutas}")
     
-    break
+print("----- fim do exercício03.py -----")
 # Exercício 03 - Python Básico com Listas
