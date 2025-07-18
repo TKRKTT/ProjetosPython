612425a649604db053e3d06c4faac41684bab480
Richard Gabryell Caetano Barbosa <richardgabryellcaetano@gmail.com>
2025-07-18 08:29:08 -0300

    add: create Questão01.py for user input and output demonstration

---
+++ Mini-Prova de Python_01/Questão01.py
@@ -0,0 +1,7 @@
+nome = input("Qual é o seu nome?")
+
+esporte_favorito = input("Qual é o seu esrporte favorito?")
+
+quantidade = int(input("Quantas vezes por semana vocẽ pratica?"))
+
+print(f" --- Olá {nome}, seu esporte favorito é {esporte_favorito} e vocẽ pratica {quantidade} vezes por semana ---") 
