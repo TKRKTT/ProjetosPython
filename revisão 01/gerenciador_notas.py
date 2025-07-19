fc29a0e6a338dcdb275d3ddc910b66f4bbb0a1a5
Richard Gabryell Caetano Barbosa <richardgabryellcaetano@gmail.com>
2025-07-19 19:27:24 -0300

    teste - 1

--- revisão 01/gerenciador_notas.py
+++ revisão 01/gerenciador_notas.py
@@ -4,3 +4,17 @@
 alunos = []
 
 # --- Funções Auxiliares ---
+
+def calcular_media(lista_de_numeros):
+    """
+    Calcula a média de uma lista de números.
+    Recebe: uma lista de números (list).
+    Retorna: a média dos números (float).
+    """
+    if not lista_de_numeros:
+        return 0.0
+    soma = sum(lista_de_numeros)
+    quantidade = len(lista_de_numeros)
+    return soma / quantidade
+
+# --- Funções do Sistema de Notas ---
