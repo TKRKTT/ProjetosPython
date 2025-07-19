b5666562bc3cc37016248cfde164cbeb5c59ef0f
Richard Gabryell Caetano Barbosa <richardgabryellcaetano@gmail.com>
2025-07-18 23:42:27 -0300

    add: create Questão04.py with function to calculate average from a list of numbers

---
+++ Mini-Prova de Python_01/Questão04.py
@@ -0,0 +1,7 @@
+# Questão04.py - Crie uma função chamada calcular_media
+
+# a.  Receba uma lista de números como argumento.
+
+# b.  Calcule a média desses números.
+
+# c.  Retorne a média calculada.
