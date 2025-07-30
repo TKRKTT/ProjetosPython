184d67cc18e30d59f1e0a0924007325c77b8a81b
Richard Gabryell Caetano Barbosa <richardgabryellcaetano@gmail.com>
2025-07-30 01:29:34 -0300

    .

--- revisão 02/codigo_series.py
+++ revisão 02/codigo_series.py
@@ -1,3 +1,19 @@
 # codigo_series.py
 
 # --- Referente à Questão 3a ---
+def soma_iterativa_4n(n):
+    """
+    Calcula 4 + 8 + 12 + ... + 4n usando um laço for.
+    Pratica o conceito de Subprograma (Função).
+    """
+    soma = 0
+    for i in range(1, n + 1):
+        soma += 4 * i
+        return soma
+    
+def soam_por_formula_2n2_2n(n):
+    """
+    Calcula o resultado da soma usando a fórmula 2n^2 + 2n. [cite: 19]
+    """
+    return 2 * (n ** 2) + 2 * n
+
